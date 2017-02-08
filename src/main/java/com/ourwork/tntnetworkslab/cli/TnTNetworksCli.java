2cfa60363d47c65a9f15e1a32f4f414d4d9c7f64
Vasilis Naskos <vnaskos@gmail.com>
2017-02-08 12:02:07 +0200

    Add cli interface for every exercise

--- src/main/java/com/ourwork/tntnetworkslab/cli/TnTNetworksCli.java
+++ src/main/java/com/ourwork/tntnetworkslab/cli/TnTNetworksCli.java
@@ -2,8 +2,14 @@ package com.ourwork.tntnetworkslab.cli;
 
 import com.ourwork.tntnetworkslab.cli.input.BasicIOManager;
 import com.ourwork.tntnetworkslab.cli.input.IOManager;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise10Solver;
 import com.ourwork.tntnetworkslab.core.solvers.Exercise1Solver;
 import com.ourwork.tntnetworkslab.core.solvers.Exercise2Solver;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise4Solver;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise6Solver;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise8Solver;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise91Solver;
+import com.ourwork.tntnetworkslab.core.solvers.Exercise92Solver;
 import com.ourwork.tntnetworkslab.core.solvers.ExerciseSolver;
 
 /**
@@ -19,14 +25,15 @@ public class TnTNetworksCli {
         iOManager = new BasicIOManager();
 
         switch (iOManager.getExercise()) {
-            case EX1:
-                solver = new Exercise1Solver();
-                break;
-            case EX2:
-                solver = new Exercise2Solver();
-                break;
-            default:
-                solver = null;
+            case EX1: solver = new Exercise1Solver(); break;
+            case EX2: solver = new Exercise2Solver(); break;
+            case EX4: solver = new Exercise4Solver(); break;
+            case EX6: solver = new Exercise6Solver(); break;
+            case EX8: solver = new Exercise8Solver(); break;
+            case EX9_1: solver = new Exercise91Solver(); break;
+            case EX9_2: solver = new Exercise92Solver(); break;
+            case EX10: solver = new Exercise10Solver(); break;
+            default: solver = null;
         }
 
         while(true) {
--- src/main/java/com/ourwork/tntnetworkslab/cli/input/BasicIOManager.java
+++ src/main/java/com/ourwork/tntnetworkslab/cli/input/BasicIOManager.java
@@ -24,12 +24,14 @@ public class BasicIOManager extends IOManager {
 
         int erg = in.nextInt();
         switch (erg) {
-            case 1:
-                exercise = Exercise.EX1;
-                break;
-            case 2:
-                exercise = Exercise.EX2;
-                break;
+            case 1: exercise = Exercise.EX1; break;
+            case 2: exercise = Exercise.EX2; break;
+            case 4: exercise = Exercise.EX4; break;
+            case 6: exercise = Exercise.EX6; break;
+            case 8: exercise = Exercise.EX8; break;
+            case 91: exercise = Exercise.EX9_1; break;
+            case 92: exercise = Exercise.EX9_2; break;
+            case 10: exercise = Exercise.EX10; break;
         }
     }
 
@@ -38,12 +40,14 @@ public class BasicIOManager extends IOManager {
         IOItem input = null;
         
         switch(exercise) {
-            case EX1:
-                input = createEx1Input();
-                break;
-            case EX2:
-                input = createEx2Input();
-                break;
+            case EX1: input = createEx1Input(); break;
+            case EX2: input = createEx2Input(); break;
+            case EX4: input = createEx4Input(); break;
+            case EX6: input = createEx6Input(); break;
+            case EX8: input = createEx8Input(); break;
+            case EX9_1: input = createEx91Input(); break;
+            case EX9_2: input = createEx92Input(); break;
+            case EX10: input = createEx10Input(); break;
         }
         
         return input;
@@ -73,16 +77,93 @@ public class BasicIOManager extends IOManager {
         
         return input;
     }
+    
+    private IOItem createEx4Input() {
+        System.out.println("\nARITMOS EKSERXOMENON GRAMMWN KAI FORTIO KINISIS:");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextInt()),
+                    new IONumeric(in.nextFloat())
+                });
+        
+        return input;
+    }
+    
+    private IOItem createEx6Input() {
+        System.out.println("\n PITHANOTHTA MPLOKARISMATOS KAI FORTIO KINHSHS: \n");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat())
+                });
+        
+        return input;
+    }
+    
+    private IOItem createEx8Input() {
+        System.out.println("\nRYTMOS AFIKSHS  RYTMOS EKSYPHRETHSHS  TETRAGVNIKH DIASPORA:\n");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat())
+                });
+        
+        return input;
+    }
+    
+    private IOItem createEx91Input() {
+        System.out.println("\nRYTMOS AFIKSHS  RYTMOS EKSYPHRETHSHS  TETRAGVNIKH DIASPORA:\n");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat())
+                });
+        
+        return input;
+    }
+    
+    private IOItem createEx92Input() {
+        System.out.println("\nRYTMOS AFIKSHS  RYTMOS EKSYPHRETHSHS  TETRAGVNIKH DIASPORA:\n");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextFloat()),
+                    new IONumeric(in.nextInt())
+                });
+        
+        return input;
+    }
+    
+    private IOItem createEx10Input() {
+        System.out.println("\nARITMOS GRAMMWN KAI FORTIO:\n");
+        
+        IOArray input = new IOArray(
+                new IONumeric[]{
+                    new IONumeric(in.nextInt()),
+                    new IONumeric(in.nextInt())
+                });
+        
+        return input;
+    }
 
     @Override
     public void outputResults(IOItem output) {
         switch (exercise) {
-            case EX1:
-                outputEx1Results(output);
-                break;
-            case EX2:
-                outputEx2Results(output);
-                break;
+            case EX1: outputEx1Results(output); break;
+            case EX2: outputEx2Results(output); break;
+            case EX4: outputEx4Results(output); break;
+            case EX6: outputEx6Results(output); break;
+            case EX8: outputEx8Results(output); break;
+            case EX9_1: outputEx91Results(output); break;
+            case EX9_2: outputEx92Results(output); break;
+            case EX10: outputEx10Results(output); break;
         }
     }
     
@@ -98,4 +179,52 @@ public class BasicIOManager extends IOManager {
                 ((IONumeric) output.next()).getValue());
     }
     
+    private void outputEx4Results(IOItem output) {
+        System.out.printf("-----------------------------------------------------------\n");
+        System.out.printf("PITHANOTHTA ANAMONIS = %.4f, M.T. KLHSEON ANAMONHS = %.4f\n", 
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+        System.out.printf("-----------------------------------------------------------\n");
+        System.out.printf("MESOS XRONOS ANAMONIS = %.4f, M.T. APODOSH GRAMMON = %.4f\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+        System.out.printf("-----------------------------------------------------------\n");
+    }
+    
+    private void outputEx6Results(IOItem output) {
+        System.out.printf("O ARITHMOS TWN GRAMMWN EINAI= %d\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+        System.out.printf("H AKRIBHS PITHANOTHTA MPLOKARISMATOS (BN)= %.5f\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+    }
+    
+    private void outputEx8Results(IOItem output) {
+        System.out.printf(" ----------------------------------------------------------------\n");
+        System.out.printf(" FORTIO KINHSHS=',%.6f,'   M.T. XRONOY ANAMONHS=',%.6f\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+        System.out.printf(" ----------------------------------------------------------------\n");
+    }
+    
+    private void outputEx91Results(IOItem output) {
+        System.out.printf(" ----------------------------------------------------------------\n");
+        System.out.printf(" FORTIO KINHSHS=',%.6f,'   M.T. XRONOY ANAMONHS=',%.6f\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+        System.out.printf(" ----------------------------------------------------------------\n");
+    }
+    
+    private void outputEx92Results(IOItem output) {
+        System.out.printf(" FORTIO KINHSHS=',%.5f,'   M.T. XRONOY ANAMONHS=',%.5f\n", 
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+    }
+    
+    private void outputEx10Results(IOItem output) {
+        System.out.printf("PITHANOTHTA MPLOKARISMATOS (B)= %.6f %%, APODOSH GRAMMWN (n)= %.6f %%\n",
+                ((IONumeric) output.next()).getValue(),
+                ((IONumeric) output.next()).getValue());
+    }
 }
